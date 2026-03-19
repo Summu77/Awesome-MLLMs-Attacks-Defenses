a96257618741bf2c3f4f41b65db38cf51fdbc7b9
Zhongqi Wang <60317828+Robin-WZQ@users.noreply.github.com>
2026-03-19 13:18:46 +0800

    Update backdoor.js

--- docs/assets/js/data/attacks/backdoor.js
+++ docs/assets/js/data/attacks/backdoor.js
@@ -1,10 +1,554 @@
 export const backdoorAttacks = [
   {
+    title: "POISONING AND BACKDOORING CONTRASTIVE LEARNING",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2021-06",
+    Tag: [],
+    link: "https://arxiv.org/abs/2106.09667v2"
+  },
+  {
+    title: "BadEncoder: Backdoor Attacks to Pre-trained Encoders in Self-Supervised Learning",
+    institution: "",
+    publication: "SSP'22",
+    publishedAt: "2021-07",
+    Tag: [],
+    link: "https://ieeexplore.ieee.org/document/9833644"
+  },
+  {
+    title: "Data Poisoning Attacks Against Multimodal Encoders",
+    institution: "",
+    publication: "ICML'23",
+    publishedAt: "2022-09",
+    Tag: [],
+    link: "https://proceedings.mlr.press/v202/yang23f.html"
+  },
+  {
+    title: "GhostEncoder: Stealthy backdoor attacks with dynamic triggers to pre-trained encoders in self-supervised learning",
+    institution: "",
+    publication: "CS'24",
+    publishedAt: "2023-10",
+    Tag: [],
+    link: "https://www.sciencedirect.com/science/article/abs/pii/S0167404824001561"
+  },
+  {
+    title: "BadCLIP: Dual-Embedding Guided Backdoor Attack on Multimodal Contrastive Learning",
+    institution: "",
+    publication: "CVPR'24",
+    publishedAt: "2023-11",
+    Tag: [],
+    link: "https://openaccess.thecvf.com/content/CVPR2024/html/Liang_BadCLIP_Dual-Embedding_Guided_Backdoor_Attack_on_Multimodal_Contrastive_Learning_CVPR_2024_paper.html"
+  },
+  {
+    title: "Distribution Preserving Backdoor Attack in Self-supervised Learning",
+    institution: "",
+    publication: "SSP'24",
+    publishedAt: "2024-05",
+    Tag: [],
+    link: "https://ieeexplore.ieee.org/abstract/document/10646825"
+  },
+  {
+    title: "BAPLe: Backdoor Attacks on Medical Foundational Models using Prompt Learning",
+    institution: "",
+    publication: "MICCAI'24",
+    publishedAt: "2024-08",
+    Tag: [],
+    link: "https://papers.miccai.org/miccai-2024/094-Paper3117.html"
+  },
+  {
+    title: "MP-Nav: Enhancing Data Poisoning Attacks against Multimodal Learning",
+    institution: "",
+    publication: "ICML'25",
+    publishedAt: "2025-03",
+    Tag: [],
+    link: "https://openreview.net/forum?id=zy7VeNtSLM"
+  },
+  {
+    title: "Backdooring CLIP through Concept Confusion",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-03",
+    Tag: [],
+    link: "https://arxiv.org/abs/2503.09095"
+  },
+  {
+    title: "Invisible Backdoor Attack against Self-supervised Learning",
+    institution: "",
+    publication: "CVPR'25",
+    publishedAt: "2025-10",
+    Tag: [],
+    link: "https://openaccess.thecvf.com/content/CVPR2025/papers/Zhang_Invisible_Backdoor_Attack_against_Self-supervised_Learning_CVPR_2025_paper.pdf"
+  },
+  {
+    title: "Backdoor in Seconds: Unlocking Vulnerabilities in Large Pre-trained Models via Model Editing",
+    institution: "",
+    publication: "CIKM'25",
+    publishedAt: "2025-11",
+    Tag: [],
+    link: "https://dl.acm.org/doi/10.1145/3746252.3761408"
+  },
+  {
+    title: "ToxicTextCLIP: Text-Based Poisoning and Backdoor Attacks on CLIP Pre-training",
+    institution: "",
+    publication: "NeurIPS'25",
+    publishedAt: "2025-11",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2511.00446"
+  },
+  {
+    title: "Backdoor Attacks on Multi-modal Contrastive Learning",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://www.arxiv.org/abs/2601.11006"
+  },
+  {
+    title: "Stealthy Backdoor Carriers: The Threat of Visual Prompts to CLIP",
+    institution: "",
+    publication: "IOTJ",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://ieeexplore.ieee.org/abstract/document/11328089"
+  },
+  {
+    title: "BadCLIP++: Stealthy and Persistent Backdoors in Multimodal Contrastive Learning",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2602.17168"
+  },
+  {
+    title: "Rickrolling the Artist: Injecting Backdoors into Text Encoders for Text-to-Image Synthesis",
+    institution: "",
+    publication: "ICCV'23",
+    publishedAt: "2022-11",
+    Tag: [],
+    link: "https://arxiv.org/abs/2211.02408"
+  },
+  {
+    title: "Personalization as a Shortcut for Few-Shot Backdoor Attack against Text-to-Image Diffusion Models",
+    institution: "",
+    publication: "AAAI'24",
+    publishedAt: "2023-05",
+    Tag: [],
+    link: "https://arxiv.org/abs/2305.10701"
+  },
+  {
+    title: "Text-to-Image Diffusion Models can be Easily Backdoored through Multimodal Data Poisoning",
+    institution: "",
+    publication: "ACM MM'23",
+    publishedAt: "2023-05",
+    Tag: [],
+    link: "https://arxiv.org/abs/2305.04175"
+  },
+  {
+    title: "VillanDiffusion: A Unified Backdoor Attack Framework for Diffusion Models",
+    institution: "",
+    publication: "NeurIPS'23",
+    publishedAt: "2023-06",
+    Tag: [],
+    link: "https://arxiv.org/abs/2306.06874"
+  },
+  {
+    title: "BAGM: A Backdoor Attack for Manipulating Text-to-Image Generative Models",
+    institution: "",
+    publication: "TIFS'24",
+    publishedAt: "2023-07",
+    Tag: [],
+    link: "https://arxiv.org/abs/2307.16489"
+  },
+  {
+    title: "Backdooring Textual Inversion for Concept Censorship",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2023-08",
+    Tag: [],
+    link: "https://arxiv.org/abs/2308.10718"
+  },
+  {
+    title: "Nightshade: Prompt-Specific Poisoning Attacks on Text-to-Image Generative Models",
+    institution: "",
+    publication: "S&P'24",
+    publishedAt: "2023-10",
+    Tag: [],
+    link: "https://arxiv.org/abs/2310.13828"
+  },
+  {
+    title: "The Stronger the Diffusion Model, the Easier the Backdoor: Data Poisoning to Induce Copyright Breaches Without Adjusting Finetuning Pipeline",
+    institution: "",
+    publication: "ICML'24",
+    publishedAt: "2024-01",
+    Tag: [],
+    link: "https://arxiv.org/abs/2401.04136"
+  },
+  {
+    title: "Injecting Bias in Text-To-Image Models via Composite-Trigger Backdoors",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2024-06",
+    Tag: [],
+    link: "https://arxiv.org/abs/2406.15213"
+  },
+  {
+    title: "Control ControlNet: Multidimensional Backdoor Attack Based on ControlNet",
+    institution: "",
+    publication: "ICONIP'24",
+    publishedAt: "2024-07",
+    Tag: [],
+    link: "https://easychair.org/publications/preprint/L1B4"
+  },
+  {
+    title: "EvilEdit: Backdooring Text-to-Image Diffusion Models in One Second",
+    institution: "",
+    publication: "ACM MM'24",
+    publishedAt: "2024-10",
+    Tag: [],
+    link: "https://dl.acm.org/doi/10.1145/3664647.3680689"
+  },
+  {
+    title: "Combinational Backdoor Attack against Customized Text-to-Image Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2024-11",
+    Tag: [],
+    link: "https://arxiv.org/abs/2411.12389"
+  },
+  {
+    title: "TrojanEdit: Backdooring Text-Based Image Editing Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2024-11",
+    Tag: [],
+    link: "https://arxiv.org/abs/2411.14681"
+  },
+  {
+    title: "Imperceptible Backdoor Attacks on Text-Guided 3D Scene Grounding",
+    institution: "",
+    publication: "TMM'25",
+    publishedAt: "2025-02",
+    Tag: [],
+    link: "https://dblp.org/rec/journals/tmm/LiuH25"
+  },
+  {
+    title: "Towards Invisible Backdoor Attack on Text-to-Image Diffusion Model",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-03",
+    Tag: [],
+    link: "https://arxiv.org/abs/2503.17724"
+  },
+  {
+    title: "Silent Branding Attack: Trigger-free Data Poisoning Attack on Text-to-Image Diffusion Models",
+    institution: "",
+    publication: "CVPR'25",
+    publishedAt: "2025-03",
+    Tag: [],
+    link: "https://arxiv.org/abs/2503.09669"
+  },
+  {
+    title: "BadVideo: Stealthy Backdoor Attack against Text-to-Video Generation",
+    institution: "",
+    publication: "ICCV'25",
+    publishedAt: "2025-04",
+    Tag: [],
+    link: "https://arxiv.org/abs/2504.16907"
+  },
+  {
+    title: "Erased but Not Forgotten: How Backdoors Compromise Concept Erasure",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-04",
+    Tag: [],
+    link: "https://arxiv.org/abs/2504.21072"
+  },
+  {
+    title: "REDEditing: Relationship-Driven Precise Backdoor Poisoning on Text-to-Image Diffusion Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-04",
+    Tag: [],
+    link: "https://arxiv.org/abs/2504.14554"
+  },
+  {
+    title: "TWIST: Text-encoder Weight-editing for Inserting Secret Trojans in Text-to-Image Models",
+    institution: "",
+    publication: "ACL'25",
+    publishedAt: "2025-06",
+    Tag: [],
+    link: "https://aclanthology.org/2025.acl-long.541/"
+  },
+  {
+    title: "Practical, Generalizable and Robust Backdoor Attacks on Text-to-Image Diffusion Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-08",
+    Tag: [],
+    link: "https://arxiv.org/abs/2508.01605"
+  },
+  {
+    title: "BadBlocks: Low-Cost and Stealthy Backdoor Attacks Tailored for Text-to-Image Diffusion Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-08",
+    Tag: [],
+    link: "https://arxiv.org/abs/2508.03221"
+  },
+  {
+    title: "Key-Value Mapping-Based Text-to-Image Diffusion Model Backdoor Attacks",
+    institution: "",
+    publication: "Algorithms",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://www.mdpi.com/1999-4893/19/1/74"
+  },
+  {
+    title: "Bad-PoseDiff: Pose-Guided Backdoor Triggering in Diffusion Models",
+    institution: "",
+    publication: "TrustCom'25",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://ieeexplore.ieee.org/abstract/document/11354876"
+  },
+  {
+    title: "Semantic-level Backdoor Attack against Text-to-Image Diffusion Models",
+    institution: "",
+    publication: "arXiv'26",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2602.04898"
+  },
+  {
+    title: "When Backdoors Go Beyond Triggers: Semantic Drift in Diffusion Models Under Encoder Attacks",
+    institution: "",
+    publication: "arXiv'26",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2602.20193"
+  },
+  {
+    title: "When LoRA Betrays: Backdooring Text-to-Image Models by Masquerading as Benign Adapters",
+    institution: "",
+    publication: "CVPR'26",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2602.21977"
+  },
+  {
+    title: "Tuning Just Enough: Lightweight Backdoor Attacks on Multi-Encoder Diffusion Models",
+    institution: "",
+    publication: "ICLRW'26",
+    publishedAt: "2026-03",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2603.04064"
+  },
+  {
+    title: "Shadowcast: Stealthy Data Poisoning Attacks against Vision-Language Models",
+    institution: "",
+    publication: "NeurIPS'24",
+    publishedAt: "2024-02",
+    Tag: [],
+    link: "https://openreview.net/forum?id=JhqyeppMiD"
+  },
+  {
+    title: "VL-Trojan: Multimodal Instruction Backdoor Attacks against Autoregressive Visual Language Models",
+    institution: "",
+    publication: "IJCV'25",
+    publishedAt: "2024-02",
+    Tag: [],
+    link: "https://link.springer.com/article/10.1007/s11263-025-02368-9"
+  },
+  {
+    title: "Test-Time BACKDOOR ATTACKS ON MULTIMODAL LARGE LANGUAGE MODELS",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2024-02",
+    Tag: [],
+    link: "https://github.com/sail-sg/AnyDoor"
+  },
+  {
+    title: "ImgTrojan: Jailbreaking Vision-Language Models with ONE Image",
+    institution: "",
+    publication: "NAACL'25",
+    publishedAt: "2024-03",
+    Tag: [],
+    link: "https://aclanthology.org/2025.naacl-long.360/"
+  },
+  {
+    title: "TrojVLM: Backdoor Attack Against Vision Language Models",
+    institution: "",
+    publication: "ECCV'24",
+    publishedAt: "2024-03",
+    Tag: [],
+    link: "https://link.springer.com/chapter/10.1007/978-3-031-73650-6_27"
+  },
+  {
+    title: "PHYSICAL BACKDOOR ATTACK CAN JEOPARDIZE DRIVING WITH VISION-LARGE-LANGUAGE MODELS",
+    institution: "",
+    publication: "ICMLW'24",
+    publishedAt: "2024-04",
+    Tag: [],
+    link: "https://icml.cc/virtual/2024/38112"
+  },
+  {
+    title: "Revisiting Backdoor Attacks against Large Vision-Language Models from Domain Shift",
+    institution: "",
+    publication: "CVPR'25",
+    publishedAt: "2024-06",
+    Tag: [],
+    link: "https://www.computer.org/csdl/proceedings-article/cvpr/2025/436400j477/2999SblAZlC"
+  },
+  {
+    title: "BACKDOORING VISION-LANGUAGE MODELS WITH OUT-OF-DISTRIBUTION DATA",
+    institution: "",
+    publication: "ICLR'25",
+    publishedAt: "2024-10",
+    Tag: [],
+    link: "https://openreview.net/forum?id=tZozeR3VV7"
+  },
+  {
+    title: "Stealthy Backdoor Attack in Self-Supervised Learning Vision Encoders for Large Vision Language Models",
+    institution: "",
+    publication: "CVPR'25",
+    publishedAt: "2025-02",
+    Tag: [],
+    link: "https://ieeexplore.ieee.org/document/11092582"
+  },
+  {
+    title: "BadToken: Token-level Backdoor Attacks to Multi-modal Large Language Models",
+    institution: "",
+    publication: "CVPR'25",
+    publishedAt: "2025-03",
+    Tag: [],
+    link: "https://www.computer.org/csdl/proceedings-article/cvpr/2025/436400ae927/299buyfnYBy"
+  },
+  {
+    title: "Natural Reflection Backdoor Attack on Vision Language Model for Autonomous Driving",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-05",
+    Tag: [],
+    link: "https://arxiv.org/abs/2505.06413"
+  },
+  {
+    title: "Backdoor Attack on Vision Language Models with Stealthy Semantic Manipulation",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-06",
+    Tag: [],
+    link: "https://arxiv.org/abs/2506.07214"
+  },
+  {
+    title: "Shadow-Activated Backdoor Attacks on Multimodal Large Language Models",
+    institution: "",
+    publication: "ACL'25",
+    publishedAt: "2025-07",
+    Tag: [],
+    link: "https://aclanthology.org/2025.findings-acl.248/"
+  },
+  {
+    title: "IAG: Input-aware Backdoor Attack on VLMs for Visual Grounding",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-08",
+    Tag: [],
+    link: "https://arxiv.org/abs/2508.09456"
+  },
+  {
+    title: "TokenSwap: Backdoor Attack on the Compositional Understanding of Large Vision-Language Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-09",
+    Tag: [],
+    link: "https://arxiv.org/abs/2509.24566"
+  },
+  {
+    title: "MTAttack: Multi-Target Backdoor Attacks against Large Vision-LanguageModels",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-11",
+    Tag: [],
+    link: "https://arxiv.org/abs/2511.10098"
+  },
+  {
+    title: "BackdoorVLM: A Benchmark for Backdoor Attacks on Vision-Language Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-11",
+    Tag: [],
+    link: "https://arxiv.org/abs/2511.18921"
+  },
+  {
     title: "BadVLA: Towards Backdoor Attacks on Vision-Language-Action Models via Objective-Decoupled Optimization",
-    institution: "University of Texas at Arlington and collaborators",
-    publication: "arXiv 2025",
+    institution: "",
+    publication: "arXiv",
     publishedAt: "2025-05",
     Tag: [],
     link: "https://arxiv.org/abs/2505.16640"
+  },
+  {
+    title: "TabVLA: Targeted Backdoor Attacks on Vision-Language-Action Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-10",
+    Tag: [],
+    link: "https://arxiv.org/abs/2510.10932"
+  },
+  {
+    title: "AttackVLA: Benchmarking Adversarial and Backdoor Attacks on Vision-Language-Action Models",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-11",
+    Tag: [],
+    link: "https://arxiv.org/abs/2511.12149"
+  },
+  {
+    title: "State Backdoor: Towards Stealthy Real-world Poisoning Attack on Vision-Language-Action Model in State Space",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://www.arxiv.org/abs/2601.04266"
+  },
+  {
+    title: "Inject Once Survive Later: Backdooring Vision-Language-Action Models to Persist Through Downstream Fine-tuning",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2602.00500"
+  },
+  {
+    title: "Hidden Ghost Hand: Unveiling Backdoor Vulnerabilities in MLLM-Powered Mobile GUI Agents",
+    institution: "",
+    publication: "EMNLP’25",
+    publishedAt: "2025-05",
+    Tag: [],
+    link: "https://aclanthology.org/2025.findings-emnlp.411/"
+  },
+  {
+    title: "Poison Once, Control Anywhere: Clean-Text Visual Backdoors in VLM-based Mobile Agents",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-06",
+    Tag: [],
+    link: "https://arxiv.org/abs/2506.13205"
+  },
+  {
+    title: "VisualTrap: A Stealthy Backdoor Attack on GUI Agents via Visual Grounding Manipulation",
+    institution: "",
+    publication: "COLM‘25",
+    publishedAt: "2025-07",
+    Tag: [],
+    link: "https://openreview.net/forum?id=7HPuAkgdVm#discussion"
+  },
+  {
+    title: "Realistic Environmental Injection Attacks on GUI Agents",
+    institution: "",
+    publication: "arXiv",
+    publishedAt: "2025-09",
+    Tag: [],
+    link: "https://arxiv.org/abs/2509.11250"
   }
 ];
