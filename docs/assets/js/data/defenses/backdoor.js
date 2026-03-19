932a2ef2c97177719782030820c6df7df3fb4094
Zhongqi Wang <60317828+Robin-WZQ@users.noreply.github.com>
2026-03-19 14:32:00 +0800

    Update backdoor.js

--- docs/assets/js/data/defenses/backdoor.js
+++ docs/assets/js/data/defenses/backdoor.js
@@ -1,1 +1,290 @@
-export const backdoorDefenses = [];
+export const backdoorDefenses = [
+  {
+    title: "ASSET: Robust Backdoor Data Detection Across a Multiplicity of Deep Learning Paradigms",
+    institution: "Virginia Tech",
+    publication: "USENIX'23",
+    publishedAt: "2023-02",
+    Tag: [],
+    link: "https://www.usenix.org/conference/usenixsecurity23/presentation/pan"
+  },
+  {
+    title: "CleanCLIP: Mitigating Data Poisoning Attacks in Multimodal Contrastive Learning",
+    institution: "University of California, Los Angeles",
+    publication: "ICCV'23",
+    publishedAt: "2023-03",
+    Tag: [],
+    link: "https://openaccess.thecvf.com/content/ICCV2023/html/Bansal_CleanCLIP_Mitigating_Data_Poisoning_Attacks_in_Multimodal_Contrastive_Learning_ICCV_2023_paper.html"
+  },
+  {
+    title: "Robust Contrastive Language-Image Pre-training against Data Poisoning and Backdoor Attacks",
+    institution: "University of California, Los Angeles",
+    publication: "NeurIPS'23",
+    publishedAt: "2023-03",
+    Tag: [],
+    link: "https://proceedings.neurips.cc/paper_files/paper/2023/hash/2232e8fee69b150005ac420bfa83d705-Abstract-Conference.html"
+  },
+  {
+    title: "Detecting Backdoors in Pre-trained Encoders",
+    institution: "Purdue University",
+    publication: "CVPR'23",
+    publishedAt: "2023-03",
+    Tag: [],
+    link: "https://openaccess.thecvf.com/content/CVPR2023/html/Feng_Detecting_Backdoors_in_Pre-Trained_Encoders_CVPR_2023_paper.html"
+  },
+  {
+    title: "Better Safe than Sorry: Pre-training CLIP against Targeted Data Poisoning and Backdoor Attacks",
+    institution: "University of California, Los Angeles",
+    publication: "ICML'24",
+    publishedAt: "2023-10",
+    Tag: [],
+    link: "https://arxiv.org/abs/2310.05862"
+  },
+  {
+    title: "Unlearning Backdoor Threats: Enhancing Backdoor Defense in Multimodal Contrastive Learning via Local Token Unlearning",
+    institution: "National University of Singapore",
+    publication: "CVPRW'24",
+    publishedAt: "2024-03",
+    Tag: [],
+    link: "https://arxiv.org/abs/2403.16257"
+  },
+  {
+    title: "Adversarial Backdoor Defense in CLIP",
+    institution: "Sun Yat-sen University",
+    publication: "arXiv",
+    publishedAt: "2024-09",
+    Tag: [],
+    link: "https://arxiv.org/abs/2409.15968"
+  },
+  {
+    title: "CleanerCLIP: Fine-grained Counterfactual Semantic Augmentation for Backdoor",
+    institution: "Institute of Information Engineering, Chinese Academy of Sciences",
+    publication: "arXiv",
+    publishedAt: "2024-09",
+    Tag: [],
+    link: "https://arxiv.org/abs/2409.17601"
+  },
+  {
+    title: "Semantic Shield: Defending Vision-Language Models Against Backdooring and Poisoning via Fine-grained Knowledge Alignment",
+    institution: "Bangladesh University of Engineering and Technology",
+    publication: "CVPR'24",
+    publishedAt: "2024-11",
+    Tag: [],
+    link: "https://openaccess.thecvf.com/content/CVPR2024/html/Ishmam_Semantic_Shield_Defending_Vision-Language_Models_Against_Backdooring_and_Poisoning_via_CVPR_2024_paper.html"
+  },
+  {
+    title: "DeDe: Detecting Backdoor Samples for SSL Encoders via Decoders",
+    institution: "The Hong Kong University of Science and Technology",
+    publication: "CVPR'25",
+    publishedAt: "2024-11",
+    Tag: [],
+    link: "https://openaccess.thecvf.com/content/CVPR2025/html/Hou_DeDe_Detecting_Backdoor_Samples_for_SSL_Encoders_via_Decoders_CVPR_2025_paper.html"
+  },
+  {
+    title: "Defending Multimodal Backdoored Models by Repulsive Visual Prompt Tuning",
+    institution: "Southeast University",
+    publication: "arXiv",
+    publishedAt: "2024-12",
+    Tag: [],
+    link: "https://arxiv.org/abs/2412.20392"
+  },
+  {
+    title: "DETECTING BACKDOOR SAMPLES IN CONTRASTIVE LANGUAGE IMAGE PRETRAINING",
+    institution: "University of Melbourne",
+    publication: "ICLR'25",
+    publishedAt: "2024-12",
+    Tag: [],
+    link: "https://iclr.cc/virtual/2025/poster/30032"
+  },
+  {
+    title: "Perturb and Recover: Fine-tuning for Effective Backdoor Removal from CLIP",
+    institution: "University of Tübingen",
+    publication: "arXiv",
+    publishedAt: "2024-12",
+    Tag: [],
+    link: "https://arxiv.org/abs/2412.00727"
+  },
+  {
+    title: "Neural Antidote: Class-Wise Prompt Tuning for Purifying Backdoors in CLIP",
+    institution: "Beihang University",
+    publication: "arXiv",
+    publishedAt: "2025-02",
+    Tag: [],
+    link: "https://arxiv.org/abs/2502.19269"
+  },
+  {
+    title: "Assimilation Matters: Model-level Backdoor Detection in Vision-Language Pretrained Models",
+    institution: "Institute of Computing Technology, Chinese Academy of Sciences",
+    publication: "arXiv",
+    publishedAt: "2025-12",
+    Tag: [],
+    link: "https://www.arxiv.org/abs/2512.00343"
+  },
+  {
+    title: "Robust defense strategies for multimodal contrastive learning: efficient fine-tuning against backdoor attacks",
+    institution: " University of Massachusetts Dartmouth",
+    publication: "Multimedia Tools and Applications",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://link.springer.com/article/10.1007/s11042-026-21339-x"
+  },
+  {
+    title: "InverTune: A Backdoor Defense Method for Multimodal Contrastive Learning via Backdoor-Adversarial Correlation Analysis",
+    institution: "Wuhan University",
+    publication: "NDSS'26",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://www.ndss-symposium.org/wp-content/uploads/2026-f1666-paper.pdf"
+  },
+  {
+    title: "UFID: A Unified Framework for Black-box Input-level Backdoor Detection on Diffusion Models",
+    institution: "University of Virginia",
+    publication: "AAAI'25",
+    publishedAt: "2024-04",
+    Tag: [],
+    link: "https://arxiv.org/abs/2404.01101"
+  },
+  {
+    title: "T2IShield: Defending Against Backdoors on Text-to-Image Diffusion Models",
+    institution: "Shanghai University of Engineering Science",
+    publication: "ECCV'24",
+    publishedAt: "2024-07",
+    Tag: [],
+    link: "https://arxiv.org/abs/2407.04215"
+  },
+  {
+    title: "Defending Text-to-image Diffusion Models: Surprising Efficacy of Textual Perturbations Against Backdoor Attacks",
+    institution: "ASUS",
+    publication: "ECCVW'24",
+    publishedAt: "2024-08",
+    Tag: [],
+    link: "https://arxiv.org/abs/2408.15721"
+  },
+  {
+    title: "Fine-grained Prompt Screening: Defending Against Backdoor Attack on Text-to-Image Diffusion Models",
+    institution: "Fudan University",
+    publication: "IJCAI'25",
+    publishedAt: "2024-11",
+    Tag: [],
+    link: "https://www.ijcai.org/proceedings/2025/0068.pdf"
+  },
+  {
+    title: "Backdoor Defense for Text Encoders in Text-to-Image Generative Models",
+    institution: "China University of Geosciences",
+    publication: "IEEE TDSC'25",
+    publishedAt: "2025-01",
+    Tag: [],
+    link: "https://www.computer.org/csdl/journal/tq/2025/06/11112743/28UtsBBMwZa"
+  },
+  {
+    title: "BackdoorDM: A Comprehensive Benchmark for Backdoor Learning in Diffusion Model",
+    institution: "The Hong Kong University of Science and Technology (Guangzhou)",
+    publication: "NeurIPS'25",
+    publishedAt: "2025-02",
+    Tag: [],
+    link: "https://arxiv.org/abs/2502.11798"
+  },
+  {
+    title: "Efficient Input-level Backdoor Detection on Text-to-Image Synthesis via Neuron Activation Variation",
+    institution: "Peking University",
+    publication: "arXiv",
+    publishedAt: "2025-03",
+    Tag: [],
+    link: "https://arxiv.org/abs/2503.06453"
+  },
+  {
+    title: "Backdoor Defense in Diffusion Models via Spatial Attention Unlearning",
+    institution: "University of Southern California",
+    publication: "arXiv",
+    publishedAt: "2025-04",
+    Tag: [],
+    link: "https://arxiv.org/abs/2504.18563"
+  },
+  {
+    title: "Dynamic Attention Analysis for Backdoor Detection in Text-to-Image Diffusion Models",
+    institution: "Institute of Computing Technology, Chinese Academy of Sciences",
+    publication: "TPAMI'25",
+    publishedAt: "2025-04",
+    Tag: [],
+    link: "https://arxiv.org/abs/2504.20518"
+  },
+  {
+    title: "On the Fairness, Diversity and Reliability of Text-to-Image Generative Models",
+    institution: "University of Western Australia",
+    publication: "Artificial Intelligence Review",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://link.springer.com/article/10.1007/s10462-025-11424-2"
+  },
+  {
+    title: "Backdoor Sentinel: Detecting and Detoxifying Backdoors in Diffusion Models via Temporal Noise Consistency",
+    institution: "Institute of Information Engineering, Chinese Academy of Sciences",
+    publication: "arXiv",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2602.01765"
+  },
+  {
+    title: "BlackMirror: Black-Box Backdoor Detection for Text-to-Image Models via Instruction-Response Deviation",
+    institution: "University of Chinese Academy of Sciences",
+    publication: "CVPR'26",
+    publishedAt: "2026-03",
+    Tag: [],
+    link: "https://arxiv.org/html/2603.05921v1"
+  },
+  {
+    title: "Backdoor Cleaning without External Guidance in MLLM Fine-tuning",
+    institution: "Wuhan University",
+    publication: "NeurIPS'25",
+    publishedAt: "2025-05",
+    Tag: [],
+    link: "https://openreview.net/forum?id=os4QYDf3Ms"
+  },
+  {
+    title: "ROBUST ANTI-BACKDOOR INSTRUCTION TUNING IN LVLMS",
+    institution: "Institute of Information Engineering, Chinese Academy of Sciences",
+    publication: "arXiv",
+    publishedAt: "2025-06",
+    Tag: [],
+    link: "https://arxiv.org/abs/2506.05401"
+  },
+  {
+    title: "SRD: Reinforcement-Learned Semantic Perturbation",
+    institution: "Wuhan University",
+    publication: "AAAI'26",
+    publishedAt: "2025-06",
+    Tag: [],
+    link: "https://www.arxiv.org/abs/2506.04743"
+  },
+  {
+    title: "From Internal Diagnosis to External Auditing: A VLM-Driven Paradigm for Online Test-Time Backdoor Defense",
+    institution: "The Chinese University of Hong Kong",
+    publication: "arXiv",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2601.19448"
+  },
+  {
+    title: "TCAP: Tri-Component Attention Profiling for Unsupervised Backdoor Detection in MLLM Fine-Tuning",
+    institution: "Shandong University",
+    publication: "arXiv",
+    publishedAt: "2026-01",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2601.21692"
+  },
+  {
+    title: "When Attention Betrays: Erasing Backdoor Attacks in Robotic Policies by Reconstructing Visual Tokens",
+    institution: "Wuhan University",
+    publication: "ICRA'26",
+    publishedAt: "2026-02",
+    Tag: [],
+    link: "https://arxiv.org/abs/2602.03153"
+  },
+  {
+    title: "Self-Purification Mitigates Backdoors in Multimodal Diffusion Language Models",
+    institution: "National University of Singapore",
+    publication: "arXiv",
+    publishedAt: "2026-03",
+    Tag: [],
+    link: "https://arxiv.org/pdf/2602.22246"
+  }
+];
